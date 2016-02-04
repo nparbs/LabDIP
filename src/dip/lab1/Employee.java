485a980a33337bb12d78d89a55bf84f789a9823d
nparbs <nparbs@L11713042776.wctc.msft>
2016-02-04 16:06:18 -0600

    start

--- src/dip/lab1/Employee.java
+++ src/dip/lab1/Employee.java
@@ -1,5 +1,7 @@
 package dip.lab1;
 
+import dip.lab1.student.solution1.*;
+
 /**
  * The concrete super class of all employee types. What's wrong with this class?
  * Find and fix all encapsulation, DIP and best practice problems learned
--- src/dip/lab1/student/solution1/Employee.java
+++ src/dip/lab1/student/solution1/Employee.java
@@ -1,5 +1,7 @@
 package dip.lab1.student.solution1;
 
+import dip.lab1.*;
+
 /**
  * The concrete super class of all employee types. What's wrong with this class?
  * Find and fix all encapsulation, DIP and best practice problems learned
@@ -7,12 +9,12 @@ package dip.lab1.student.solution1;
  *
  * @author your name goes here
  */
-public class Employee {
+public interface Employee {
     // You gotta question if these are appropriate for ALL employees?
-    private double hourlyRate;
-    private double totalHrsForYear;
-    private double annualSalary;
-    private double annualBonus;
+   // private double hourlyRate;
+   // private double totalHrsForYear;
+   // private double annualSalary;
+   // private double annualBonus;
 
     /**
      * Returns annual wages for hourly workers. Is this polymorphic?
@@ -21,74 +23,74 @@ public class Employee {
      * @return annual wages for hourly workers using the formula
      * annualWages = hourlyRate * totalHrsForYear
      */
-    public double getAnnualWages() {
-        return hourlyRate * totalHrsForYear;
-    }
+    public abstract double getAnnualWages();
+        //return hourlyRate * totalHrsForYear;
+    
 
     /**
      * Returns annual bonus for salaried workers. Is this polymorphic?
      * Could it be? Does it belong here?
      *
      * @return annual bonus or zero if none.
-     */
+    
     public double getAnnualBonus() {
         return annualBonus;
     }
-
+ */
     /**
      * Is this polymorphic? Should it be? Does it belong here?
      * @param annualBonus -- think carefully about this
-     */
+     
     public void setAnnualBonus(double annualBonus) {
         this.annualBonus = annualBonus;
     }
-
+*/
     /**
      * Is this polymorphic? Should it be? Does it belong here?
      * @return
-     */
+    
     public double getAnnualSalary() {
         return annualSalary;
     }
-
+ */
     /**
      * Is this polymorphic? Should it be? Does it belong here?
      * @param annualSalary - think carefully about this
-     */
+     
     public void setAnnualSalary(double annualSalary) {
         this.annualSalary = annualSalary;
     }
-
+*/
     /**
      * Is this polymorphic? Should it be? Does it belong here?
      * @return
-     */
+     
     public double getHourlyRate() {
         return hourlyRate;
     }
-
+*/
     /**
      * Is this polymorphic? Should it be? Does it belong here?
      * @param hourlyRate -- think carefully about this
-     */
+     
     public void setHourlyRate(double hourlyRate) {
         this.hourlyRate = hourlyRate;
     }
-
+*/
     /**
      * Is this polymorphic? Should it be? Does it belong here?
      * @return
-     */
+    
     public double getTotalHrsForYear() {
         return totalHrsForYear;
     }
-
+ */
     /**
      * Is this polymorphic? Should it be? Does it belong here?
      * @param totalHrsForYear -- think carefully about this
-     */
+     
     public void setTotalHrsForYear(double totalHrsForYear) {
         this.totalHrsForYear = totalHrsForYear;
     }
-
+*/
 }
