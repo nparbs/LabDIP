5a3e6321af349f7335f900db787b69438cf67c50
Nick <Nick@NParbsLaptop>
2016-02-11 13:25:10 -0600

    lab2 fix

--- src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
+++ src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
@@ -11,12 +11,12 @@ import dip.lab2.*;
  * @author your name goes here
  */
 public class FoodServiceTipCalculator implements TipCalculator {
-    private double MIN_BILL = 0.00;
-    private String BILL_ENTRY_ERR =
-            "Error: bill must be greater than or equal to " + MIN_BILL;
-    private double GOOD_RATE = 0.20;
-    private double FAIR_RATE = 0.15;
-    private double POOR_RATE = 0.10;
+    private double minBill = 0.00;
+    private String billError =
+            "Error: bill must be greater than or equal to " + minBill;
+    private double goodRate = 0.20;
+    private double fairRate = 0.15;
+    private double poorRate = 0.10;
 
     private double bill;
 
@@ -33,13 +33,13 @@ public class FoodServiceTipCalculator implements TipCalculator {
 
         switch(serviceQuality) {
             case GOOD:
-                tip = bill * GOOD_RATE;
+                tip = bill * goodRate;
                 break;
             case FAIR:
-                tip = bill * FAIR_RATE;
+                tip = bill * fairRate;
                 break;
             case POOR:
-                tip = bill * POOR_RATE;
+                tip = bill * poorRate;
                 break;
         }
 
@@ -47,8 +47,8 @@ public class FoodServiceTipCalculator implements TipCalculator {
     }
 
     public final void setBill(double billAmt) {
-        if(billAmt < MIN_BILL) {
-            throw new IllegalArgumentException(BILL_ENTRY_ERR);
+        if(billAmt < minBill) {
+            throw new IllegalArgumentException(billError);
         }
         bill = billAmt;
     }
